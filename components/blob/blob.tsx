4c8172bf2a7f87c475b42ddaf3d2a0c8134d4d7b
Bradley Love <brad@bradlove.co.za>
2022-06-07 11:57:44 +0200

    Update blob.tsx

--- components/blob/blob.tsx
+++ components/blob/blob.tsx
@@ -1,6 +1,5 @@
 
 import React, {
-    useEffect,
     useMemo,
     useRef
 } from "react";
@@ -13,10 +12,7 @@ import {
     MeshPhysicalMaterial,
     Vector3
 } from "three";
-import {
-    useMotionValue,
-    useViewportScroll
-} from "framer-motion";
+import { useViewportScroll } from "framer-motion";
 import { makeNoise3D } from "fast-simplex-noise";
 import { Select } from "@react-three/postprocessing";
 import { meshBounds } from "@react-three/drei";
@@ -30,7 +26,7 @@ const BLOB_INITIAL_SIZE: [
     radius: number,
     widthSegments: number,
     heightSegments: number
-] = [BLOB_SIZE, 32, 32];
+] = [BLOB_SIZE, 64, 64];
 const BLOB_INITIAL_SCALE = new Vector3(0, 0, 0);
 const BLOB_INITIAL_POSITION = new Vector3(0, 0, -10);
 
@@ -42,8 +38,6 @@ export const Blob: React.ComponentType<BlobProps> = ({
     blobColor
 }) => {
 
-    const numberOfSpikes = useMotionValue(0);
-    const spikeSize = useMotionValue(0);
     const blob = useRef<Mesh>(undefined!);
     const positionVector = useRef<Vector3>(new Vector3(0, 0, 0));
     const scaleVector = useRef<Vector3>(new Vector3());
@@ -58,54 +52,33 @@ export const Blob: React.ComponentType<BlobProps> = ({
 
     const scale = useMemo(() => viewport.width > viewport.height ? viewport.aspect * 0.825 : viewport.aspect * 1.4, [viewport]);
 
-    useEffect(() => {
-
-        const updateMotionValues = (): void => {
-
-            numberOfSpikes.set(Math.cos(1.25 * scrollYProgress.get()));
-            spikeSize.set(Math.sin(scrollYProgress.get() * 2));
-
-        };
-
-        const unsubscribeScrollYProgress = scrollYProgress.onChange(updateMotionValues);
-
-        return () => {
-            unsubscribeScrollYProgress();
-        };
-
-    }, []);
-
     useFrame(({
-        gl,
-        scene,
-        camera,
         clock
     }, delta) => {
 
         const speed = delta * 2;
         const time = clock.getElapsedTime() * 0.025;
+        const numberOfSpikes = Math.cos(1.25 * scrollYProgress.get());
+        const spikeSize = Math.sin(scrollYProgress.get() * 2);
 
         scaleVector.current.setScalar(scale);
 
         blob.current.scale.lerp(scaleVector.current, speed);
         blob.current.position.lerp(positionVector.current.setScalar(0), speed);
-        console.log(material.current.color.equals(color));
         material.current.color.lerp(color, speed);
 
         const position = blob.current.geometry.getAttribute("position");
-        const positionArray = position.array;
-        const normal = blob.current.geometry.getAttribute("normal");
 
         // eslint-disable-next-line more/no-c-like-loops -- array is of type ArrayLike so this is easier
-        for(let index = 0; index < positionArray.length; index++){
+        for(let index = 0; index < position.count; index++){
 
             positionVector.current.fromBufferAttribute(position, index);
             positionVector.current.normalize();
             positionVector.current.multiplyScalar(
-                BLOB_SIZE + spikeSize.get() * noise(
-                    positionVector.current.x * numberOfSpikes.get() + time,
-                    positionVector.current.y * numberOfSpikes.get() + time,
-                    positionVector.current.z * numberOfSpikes.get() + time
+                BLOB_SIZE + spikeSize * noise(
+                    positionVector.current.x * numberOfSpikes + time,
+                    positionVector.current.y * numberOfSpikes + time,
+                    positionVector.current.z * numberOfSpikes + time
                 )
             );
             position.setXYZ(index, positionVector.current.x, positionVector.current.y, positionVector.current.z);
@@ -114,9 +87,6 @@ export const Blob: React.ComponentType<BlobProps> = ({
 
         blob.current.geometry.computeVertexNormals();
         position.needsUpdate = true;
-        normal.needsUpdate = true;
-
-        gl.render(scene, camera);
 
         return undefined;
 
