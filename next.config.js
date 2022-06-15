7036e4d3a2ff9c30312634a8fabbe38653064154
Bradley Love <brad@bradlove.co.za>
2022-06-15 10:50:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,12 @@ const stylelintWebpackPlugin = require('stylelint-webpack-plugin');
 const withTM = require('next-transpile-modules')(['three']);
 
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
+    productionBrowserSourceMaps: true,
+    reactStrictMode: true,
+    webpack: (config) => {
       config.plugins.push(new stylelintWebpackPlugin());
       return config;
-  }
+    }
 }
 
 module.exports = withTM()
