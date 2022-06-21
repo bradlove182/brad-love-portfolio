33e5298d547dadb896ec779d4e4a7cfb0f9dd980
Bradley Love <brad@bradlove.co.za>
2022-06-21 16:34:19 +0200

    footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 import style from "./index.module.scss";
 
+const currentYear = new Date().getFullYear();
+
 export const Footer: React.ComponentType = () => (
     <div className={ style.footer }>
-        { `© Bradley Love ${ new Date().getFullYear() }` }
+        { `© Bradley Love ${ currentYear }` }
     </div>
 );
