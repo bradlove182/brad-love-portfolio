e7bb46256bb7598286ee3a4aaf02ff4e788e136d
Bradley Love <brad@bradlove.co.za>
2022-06-22 16:17:43 +0200

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { ScrollingText } from "../components/scrolling-text";
-import { ScrollIndicator } from "../components/scroll-indicator";
 import { About } from "../components/about";
 import { RecentProjects } from "../components/recent-projects";
 import { Contact } from "../components/contact";
@@ -11,7 +10,6 @@ import type { NextPage } from "next";
 const Home: NextPage = () => (
     <React.Fragment>
         <ScrollingText />
-        <ScrollIndicator />
         <About />
         <RecentProjects />
         <Contact />
